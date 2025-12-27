4b81ba2d9c162a4269bbd653a80c925a2f90e6e2
scripts-for-mistik <svyatiktest20@gmail.com>
2025-12-27 12:54:47 +0200

    Update userinstall.sh

--- userinstall.sh
+++ userinstall.sh
@@ -11,6 +11,7 @@ yay -S xnviewmp
 git clone https://github.com/scripts-for-mistik/dotfiles.git
 cd dotfiles
 cp -r ./ /home/mistik
+mkdir /home/mistik/temp
 
 cd /home/mistik
 git clone https://github.com/scripts-for-mistik/scripts.git
@@ -20,3 +21,12 @@ cd /home/mistik/scripts/mcmap
 chmod +x *
 chmod +x /home/mistik/scripts/mini_programs/cubiomes-viewer
 chmod +x /home/mistik/scripts/mini_programs/screenshot
+
+cd /home/mistik/scripts/mcaselector
+7z x zulu.zip
+rm zulu.z*
+mkdir zulu
+mv zulu.7z ./zulu
+cd zulu
+7z x zulu.7z
+rm zulu.7z
